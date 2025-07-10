bda35164b7db8171c874419777c0c9664035fee4
CharifMah <charif25430@hotmail.fr>
2025-07-10 18:21:07 +0200

    update charts

--- src/LevelConfig.java
+++ src/LevelConfig.java
@@ -9,7 +9,7 @@ import engine.core.MarioAgent;
 public class LevelConfig {
 	public final static MarioAgent Agent = new agents.robinBaumgarten.Agent();
 	
-	public final static String LevelPath = "./levels/generated/GANGeneratorTF/9.txt";
+	public final static String LevelPath = "./levels/generated/GANGeneratorTF/72.txt";
 	
 	public final static Boolean isBatchPlay = false;
 	
